68c4eb82e5f0db57bf3ad9a01fef130e1c4d6f7c
Simon Langebrake <s.langebrake@gmail.com>
2024-09-12 21:45:00 +0200

    Add translations

--- portal_jdav_bayern/settings.py
+++ portal_jdav_bayern/settings.py
@@ -122,13 +122,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'de'
 
 TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [ BASE_DIR / 'locale' ]
 
 
 # Static files (CSS, JavaScript, Images)
--- sections/models.py
+++ sections/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Federation(models.Model):
@@ -6,29 +7,33 @@ class Federation(models.Model):
     STATE = 1
     DISTRICT = 2
     TYPE_CHOICES = {
-        NATIONAL: 'Bundesverband',
-        STATE: 'Landesverband',
-        DISTRICT: 'Bezirk',
+        NATIONAL: _('National Federation'),
+        STATE: _('State Federation'),
+        DISTRICT: _('District Federation'),
     }
 
-    type = models.IntegerField(choices=TYPE_CHOICES)
-    name = models.CharField(max_length=200)
-    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True, blank=True)
+    type = models.IntegerField(_('Level'), choices=TYPE_CHOICES)
+    name = models.CharField(_('Name'), max_length=200)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, verbose_name=_('Parent'), null=True, blank=True)
 
     def __str__(self):
         return self.TYPE_CHOICES[self.type] + ' ' + self.name
     
     class Meta:
         ordering = ['type', 'name']
+        verbose_name = _('Federation')
+        verbose_name_plural = _('Federations')
 
 class Section(models.Model):
-    number = models.IntegerField(unique=True)
-    name = models.CharField(max_length=200)
-    state = models.ForeignKey(Federation, related_name='state_section_set', on_delete=models.PROTECT)
-    district = models.ForeignKey(Federation, related_name='district_section_set', on_delete=models.PROTECT, null=True, blank=True)
+    number = models.IntegerField(_('Number'), unique=True)
+    name = models.CharField(_('Name'), max_length=200)
+    state = models.ForeignKey(Federation, on_delete=models.PROTECT, related_name='state_section_set', verbose_name=_('State Federation'))
+    district = models.ForeignKey(Federation, on_delete=models.PROTECT, related_name='district_section_set', verbose_name=_('District Federation'), null=True, blank=True)
 
     def __str__(self):
         return self.name
     
     class Meta:
         ordering = ['name']
+        verbose_name = _('Section')
+        verbose_name_plural = _('Sections')
