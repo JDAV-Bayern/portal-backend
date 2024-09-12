3bac869931d7963a7ee1897f30dcc1fa025c6f1c
Simon Langebrake <s.langebrake@gmail.com>
2024-09-12 17:27:27 +0200

    Add endpoint for sections in a federation

--- sections/urls.py
+++ sections/urls.py
@@ -6,6 +6,7 @@ from sections import views
 urlpatterns = [
     path('federations/', views.FederationList.as_view(), name='federation-list'),
     path('federations/<int:pk>/', views.FederationDetail.as_view(), name='federation-detail'),
+    path('federations/<int:federation_id>/sections/', views.FederationSectionList.as_view(), name='federation-section-list'),
     path('sections/', views.SectionList.as_view(), name='section-list'),
     path('sections/<int:pk>/', views.SectionDetail.as_view(), name='section-detail'),
 ]
--- sections/views.py
+++ sections/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from sections.models import Federation, Section
@@ -11,6 +12,20 @@ class FederationList(generics.ListAPIView):
 class FederationDetail(generics.RetrieveAPIView):
     queryset = Federation.objects.all()
     serializer_class = FederationSerializer
+
+class FederationSectionList(generics.ListAPIView):
+    serializer_class = SectionSerializer
+
+    def get_queryset(self):
+        federation = get_object_or_404(Federation, pk=self.kwargs['federation_id'])
+        if federation.type == Federation.NATIONAL:
+            return Section.objects.all()
+        elif federation.type == Federation.STATE:
+            return Section.objects.filter(state=federation)
+        elif federation.type == Federation.DISTRICT:
+            return Section.objects.filter(district=federation)
+        else:
+            return Section.objects.none()
 
 class SectionList(generics.ListAPIView):
     queryset = Section.objects.all()
