487a45730ef724941fd8603299cd49232d0b49ff
Simon Langebrake <s.langebrake@gmail.com>
2024-10-04 22:09:06 +0200

    Fix person serializer

--- contact_info/serializers.py
+++ contact_info/serializers.py
@@ -13,19 +13,3 @@ class LocalitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Locality
         fields = ['postal_code', 'name', 'country']
-
-
-class AddressSerializer(serializers.ModelSerializer):
-    postal_code = serializers.CharField(source='locality.postal_code')
-    locality = serializers.CharField(source='locality.name')
-    country = serializers.PrimaryKeyRelatedField(source='locality.country', queryset=Country.objects.all())
-
-    class Meta:
-        model = Address
-        fields = ['line1', 'line2', 'postal_code', 'locality', 'country']
-
-
-class BankAccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BankAccount
-        fields = ['iban', 'bic']
--- people/serializers.py
+++ people/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 
-from contact_info.serializers import AddressSerializer, BankAccountSerializer
-from people.models import Person
+from contact_info.models import Country
+from people.models import Person, PersonAddress, PersonBankAccount
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    postal_code = serializers.CharField(source='locality.postal_code')
+    locality = serializers.CharField(source='locality.name')
+    country = serializers.PrimaryKeyRelatedField(source='locality.country', queryset=Country.objects.all())
+
+    class Meta:
+        model = PersonAddress
+        fields = ['line1', 'line2', 'postal_code', 'locality', 'country']
+
+
+class BankAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PersonBankAccount
+        fields = ['iban', 'bic']
 
 
 class PersonSerializer(serializers.ModelSerializer):
