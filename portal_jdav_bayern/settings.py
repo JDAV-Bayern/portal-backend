e72df1b889437713545f57ae14629984fa1b509a
joseph <joseph@maxau.de>
2024-11-16 14:05:04 +0100

    add docs

--- portal_jdav_bayern/settings.py
+++ portal_jdav_bayern/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from enum import Enum
 import json
 import logging
 import os
@@ -64,6 +65,7 @@ INSTALLED_APPS = [
     'people',
     'courses',
     'reimbursements',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -158,3 +160,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+class Environments(Enum):
+    DEVELOPMENT = 'development'
+    PRODUCTION = 'production'
+
+ENVIRONMENT = Environments.DEVELOPMENT if os.getenv('PORTAL_JDAV_BAYERN_ENV') == 'development' else Environments.PRODUCTION
--- portal_jdav_bayern/urls.py
+++ portal_jdav_bayern/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from portal_jdav_bayern import settings
 
 
 urlpatterns = [
@@ -25,6 +28,14 @@ urlpatterns = [
     path('api/', include('reimbursements.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.ENVIRONMENT == settings.Environments.DEVELOPMENT:
+    urlpatterns += [
+        path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    ]
+
+
 
 
 admin.site.site_header = 'Verwaltungsportal'
